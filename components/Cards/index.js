a6c61e0084a313794742cc5ada5e083d9ad85dd0
Conary36 <cbeckford144@gmail.com>
2019-11-10 22:54:03 -0500

    first commit for sprint

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,59 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+// const itemsList = [
+//     [javascript],
+//     [bootstrap],
+//     [technology],
+//     [jquery],
+//     [node]
+// ];
+
+const newArticles = (items) => {
+    //component elements
+    const card = document.createElement('div');
+    const headLine = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
+
+    //classes
+    card.classList.add('card');
+    headLine.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    //appending elements
+    card.append(headLine, author);
+    author.append(imgContainer, authorName);
+    imgContainer.appendChild(img);
+
+    //text Content
+
+    headLine.textContent = items.headline;
+    img.src = items.authorPhoto;
+    authorName.textContent = items.authorName;
+
+    return card;
+
+}
+
+const newsCards = document.querySelector('.cards-container');
+
+axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(response => {
+        const theseKeys = Object.keys(response.data.articles);
+
+        theseKeys.forEach(alpha => {
+            response.data.articles[alpha].forEach(items => {
+                console.log(response.data)
+                const chosenCard = newArticles(items);
+                newsCards.append(chosenCard);
+            })
+        })
+    })
+    .catch(err => {
+        console.log('O MY GOSH!', err)
+    });
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+function Header() {
+    const header = document.createElement('div');
+    const headerDate = document.createElement('span');
+    const headerTitle = document.createElement('h1');
+    const headerTemp = document.createElement('span');
+
+    header.classList.add('header');
+    headerDate.classList.add('date');
+    headerTemp.classList.add('temp');
+
+    headerDate.textContent = `SMARCH 28, 2019`;
+    headerTitle.textContent = `Lambda Times`;
+    headerTemp.textContent = `98°`;
+
+    header.append(headerDate, headerTitle, headerTemp);
+
+    return header;
+
+
+}
+
+headerContainer.append(Header());
